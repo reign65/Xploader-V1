c839146f03216d2dc53567ae54fc04f2c6bfce3c
heyit-tylor <163938073+heyit-tylor@users.noreply.github.com>
2024-09-29 12:23:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+////
+//project_name : XPLOADER 
+// @author : TYLOR
+// @youtube : https://www.youtube.com/@heyits_tylor
+// @instagram  : heyits_tylor
+// @telegram : t.me/heyits_tylor
+// @github : heyit-tylor
+// @tiktok :hey.its_tylor
+// @whatsapp : +254796180105
+//*
+//*
+//base script by DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/ahmmitech
+//GitHub: @salmanytofficial
+//WhatsApp: +923184070915
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
